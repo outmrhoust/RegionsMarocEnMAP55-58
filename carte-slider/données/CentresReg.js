711d2964de87a8fbb0601c1cbb681fc3b930fbf6
outmrhoust <60301533+outmrhoust@users.noreply.github.com>
2020-01-25 23:35:44 +0000

    initial commit

---
+++ carte-slider/données/CentresReg.js
@@ -0,0 +1,19 @@
+{
+"type": "FeatureCollection",
+"name": "CentresReg",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Tanger Tetouan Al-Houceima", "taux_d_ana": 69.0, "PIB_par_se": 55.1, "pib_par__1": 32.0, "pib_par__2": 12.9, "Pib_par_ha": 24965.0, "raccordeme": 93.5, "raccorde_1": 63.9 }, "geometry": { "type": "Point", "coordinates": [ -5.641618108276387, 35.351591028082076 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "L'oriental", "taux_d_ana": 65.6, "PIB_par_se": 68.6, "pib_par__1": 16.7, "pib_par__2": 14.8, "Pib_par_ha": 28865.0, "raccordeme": 88.9, "raccorde_1": 68.3 }, "geometry": { "type": "Point", "coordinates": [ -2.859630404199302, 34.205211389916307 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Fes-Meknes", "taux_d_ana": 65.3, "PIB_par_se": 63.7, "pib_par__1": 13.9, "pib_par__2": 22.4, "Pib_par_ha": 22252.0, "raccordeme": 92.2, "raccorde_1": 69.8 }, "geometry": { "type": "Point", "coordinates": [ -5.179902424833843, 33.540601364396252 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Rabat-Sale-Kenitra", "taux_d_ana": 70.6, "PIB_par_se": 70.1, "pib_par__1": 17.1, "pib_par__2": 12.9, "Pib_par_ha": 34313.0, "raccordeme": 90.4, "raccorde_1": 72.9 }, "geometry": { "type": "Point", "coordinates": [ -6.522771515735733, 33.823317906531386 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Marrakech Safi", "taux_d_ana": 62.2, "PIB_par_se": 65.3, "pib_par__1": 21.1, "pib_par__2": 13.6, "Pib_par_ha": 26197.0, "raccordeme": 91.1, "raccorde_1": 72.8 }, "geometry": { "type": "Point", "coordinates": [ -9.213509744898756, 31.525532704978229 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Souss Massa", "taux_d_ana": 66.9, "PIB_par_se": 57.2, "pib_par__1": 15.6, "pib_par__2": 27.1, "Pib_par_ha": 26328.0, "raccordeme": 92.6, "raccorde_1": 83.7 }, "geometry": { "type": "Point", "coordinates": [ -9.211952428931191, 29.915006891825637 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Guelmim-Oued Noun", "taux_d_ana": 67.1, "PIB_par_se": 70.0, "pib_par__1": 13.0, "pib_par__2": 17.1, "Pib_par_ha": 26286.0, "raccordeme": 93.7, "raccorde_1": 79.3 }, "geometry": { "type": "Point", "coordinates": [ -10.669783854677329, 28.307866511125038 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Laayoune-Sakia El Hamra", "taux_d_ana": 78.5, "PIB_par_se": 38.3, "pib_par__1": 53.2, "pib_par__2": 8.5, "Pib_par_ha": 90803.0, "raccordeme": 92.3, "raccorde_1": 81.6 }, "geometry": { "type": "Point", "coordinates": [ -13.313662532672286, 26.207614606136982 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Dakhla Oued Ed-Dahab", "taux_d_ana": 77.7, "PIB_par_se": 44.7, "pib_par__1": 12.1, "pib_par__2": 43.2, "Pib_par_ha": 51826.0, "raccordeme": 84.7, "raccorde_1": 84.5 }, "geometry": { "type": "Point", "coordinates": [ -15.171898433208687, 22.692323104671623 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Draa-Tafilalet", "taux_d_ana": 65.5, "PIB_par_se": 63.3, "pib_par__1": 15.9, "pib_par__2": 20.8, "Pib_par_ha": 26328.0, "raccordeme": 92.4, "raccorde_1": 81.9 }, "geometry": { "type": "Point", "coordinates": [ -6.48619837784685, 30.890343406013276 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Beni Mellal Khenifra", "taux_d_ana": 60.9, "PIB_par_se": 49.3, "pib_par__1": 31.3, "pib_par__2": 19.3, "Pib_par_ha": 28021.0, "raccordeme": 85.2, "raccorde_1": 70.1 }, "geometry": { "type": "Point", "coordinates": [ -6.672330223415648, 32.268790947480255 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "regions": "Casablanca Settat", "taux_d_ana": 73.8, "PIB_par_se": 44.0, "pib_par__1": 51.8, "pib_par__2": 4.2, "Pib_par_ha": 39513.0, "raccordeme": 93.7, "raccorde_1": 75.4 }, "geometry": { "type": "Point", "coordinates": [ -8.507466375390866, 32.928377934953176 ] } }
+]
+}
---
+++ données/CentresRegions.js
@@ -0,0 +1,19 @@
+{
+"type": "FeatureCollection",
+"name": "CentresRegions",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -5.641618108276387, 35.351591028082069 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -2.859630404199302, 34.205211389916293 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -5.179902424833843, 33.540601364396267 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -6.522771515735733, 33.823317906531386 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -9.213509744898756, 31.525532704978229 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -9.211952428931191, 29.915006891825637 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -10.669783854677329, 28.307866511125052 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -13.313662532672286, 26.207614606136982 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -15.171898433208687, 22.692323104671608 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -6.48619837784685, 30.890343406013262 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -6.672330223415648, 32.268790947480234 ] } },
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "Point", "coordinates": [ -8.507466375390866, 32.928377934953176 ] } }
+]
+}
